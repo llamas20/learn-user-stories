347e3e8c5fa52f9c2d3bebaaf56be9d947199865
Jerry Vogel <vogel.jer@northeastern.edu>
2024-09-14 15:43:35 -0400

    Resolved issues #2, #3, #4

--- bank-demo/src/bank.ts
+++ bank-demo/src/bank.ts
@@ -20,9 +20,72 @@ export default class Bank {
     private findAccount(accountNumber: string): BankAccount | undefined {
         return this.accounts.find(account => account.accountNumber === accountNumber);
     }
-    
-    public depositMoney(amount: number): void {
 
+    /**
+     * Method to find a bank account in the bank with more credentials
+     * @param name Account holder name of the bank account to find
+     * @param accountNumber Account number of the bank account to find
+     * @returns Bank account if found, undefined otherwise
+     */
+    private findAccountWithName(name: string, accountNumber: string): BankAccount | undefined {
+        return this.accounts.find(account => account.accountNumber === accountNumber && account.name === name);
+    }
+
+    /**
+     * Method to deposit money into a bank account given valid credentials
+     * @param name Account holder name of the bank account to deposit into
+     * @param account Account number of the bank account to deposit into
+     * @param amount Amount to deposit into the account.  Must be positive number.
+     */
+    public depositMoney(name: string, account: string, amount: number): void {
+        const isAccExists = this.findAccountWithName(name, account);
+        if(amount > 0) {
+            if(isAccExists) {
+                isAccExists.balance += amount;
+            } else {
+                throw new Error("Invalid account credentials")
+            }
+        } else {
+            throw new Error("Invalid deposit amount")
+        }
+    }
+
+     /**
+     * Method to withdraw money into a bank account given valid credentials
+     * @param name Account holder name of the bank account to withdraw into
+     * @param account Account number of the bank account to withdraw into
+     * @param amount Amount to withdraw into the account.  Must be positive number.
+     */
+    public withdrawMoney(name: string, account: string, amount: number): void {
+        const isAccExists = this.findAccountWithName(name, account);
+        if(amount > 0) {
+            if(isAccExists) {
+                if(isAccExists.balance - amount <= 0) {
+                    throw new Error("Invalid withdrawal amount")
+                } else {
+                    isAccExists.balance -= amount;
+                }
+            } else {
+                throw new Error("Invalid account credentials")
+            }
+        } else {
+            throw new Error("Invalid withdrawal amount")
+        }
+    }
+
+    /**
+     * Checks the balance for the given 
+     * @param name Account holder name for the account to be checked
+     * @param account Account number for the account to be checked
+     * @returns The balance for the account.  Error if the account credentials are invalid
+     */
+    public checkBalance(name: string, account: string): number {
+        const isAccExists = this.findAccountWithName(name, account);
+        if(isAccExists) {
+            return isAccExists.balance
+        } else {
+            throw new Error("Invalid account credentials");
+        }
     }
 
     /**
--- bank-demo/tests/bankTest.ts
+++ bank-demo/tests/bankTest.ts
@@ -3,18 +3,177 @@ import Bank from "../src/bank";
 // setup
 const bank = new Bank();
 
-// Scenario 1
+// Issue 1, Scenario 1
 const account = bank.createAccount("John Doe", 29, "587432");
 if(account.accountNumber === "587432") {
-    console.log("Scenario 1 Passed");
+    console.log("Scenario 1.1 Passed");
 } else {
-    console.log("Scenario 1 Failed");
+    console.log("Scenario 1.1 Failed");
 }
 
-// Scenario 2
+// Issue 1, Scenario 2
 try {
     const account = bank.createAccount("John Doe", 29, "587432");
-    console.log("Scenario 2 Failed");
+    console.log("Scenario 1.2 Failed");
 } catch(_) {
-    console.log("Scenario 2 Passed");
+    console.log("Scenario 1.2 Passed");
+}
+
+// Issue 2, Scenario 1 & Issue 4, Scenario 1
+bank.depositMoney("John Doe", "587432", 500);
+if(bank.checkBalance("John Doe", "587432") == 500) {
+    console.log("Scenarios 2.1, 4.1 Pass");
+} else {
+    console.log("Scenarios 2.1, 4.1 Failed");
+}
+
+// Issue 2, Scenario 1 & Issue 4, Scenario 1 additional test
+bank.depositMoney("John Doe", "587432", 78.25);
+if(bank.checkBalance("John Doe", "587432") == 578.25) {
+    console.log("Scenarios 2.1, 4.1 Pass");
+} else {
+    console.log("Scenarios 2.1, 4.1 Failed");
+}
+
+// Issue 2, Scenario 2
+try {
+    bank.depositMoney("Bob Smith", "587432", 500);
+    console.log("Scenario 2.2 Failed");
+} catch(_) {
+    console.log("Scenario 2.2 Pass");
+}
+
+// Issue 2, Scenario 2 Alt
+try {
+    bank.depositMoney("John Doe", "123456", 500);
+    console.log("Scenario 2.2 Failed");
+} catch(_) {
+    console.log("Scenario 2.2 Pass");
+}
+
+// Issue 2, Scenario 2 Alt
+try {
+    bank.depositMoney("John Doe", "5874321", 500);
+    console.log("Scenario 2.2 Failed");
+} catch(_) {
+    console.log("Scenario 2.2 Pass");
+}
+
+// Issue 2, Scenario 2 Alt
+try {
+    bank.depositMoney("Jon Doe", "123456", 500);
+    console.log("Scenario 2.2 Failed");
+} catch(_) {
+    console.log("Scenario 2.2 Pass");
+}
+
+// Issue 2, Scenario 3
+try {
+    bank.depositMoney("John Doe", "587432", -500);
+    console.log("Scenario 2.3 Failed");
+} catch(_) {
+    console.log("Scenario 2.3 Pass");
+}
+
+// Issue 2, Scenario 3
+try {
+    bank.depositMoney("John Doe", "587432", 0);
+    console.log("Scenario 2.3 Failed");
+} catch(_) {
+    console.log("Scenario 2.3 Pass");
+}
+
+// Issue 3, Scenario 1 & Issue 4, Scenario 1
+bank.withdrawMoney("John Doe", "587432", 100);
+if(bank.checkBalance("John Doe", "587432") == 478.25) {
+    console.log("Scenarios 3.1, 4.1 Pass");
+} else {
+    console.log("Scenarios 3.1, 4.1 Failed");
+}
+
+// Issue 3, Scenario 1 & Issue 4, Scenario 1 additional test
+bank.withdrawMoney("John Doe", "587432", 78.25);
+if(bank.checkBalance("John Doe", "587432") == 400) {
+    console.log("Scenarios 3.1, 4.1 Pass");
+} else {
+    console.log("Scenarios 3.1, 4.1 Failed");
+}
+
+// Issue 3, Scenario 2
+try {
+    bank.withdrawMoney("Bob Smith", "587432", 500);
+    console.log("Scenario 3.2 Failed");
+} catch(_) {
+    console.log("Scenario 3.2 Pass");
+}
+
+// Issue 3, Scenario 2 Alt
+try {
+    bank.withdrawMoney("John Doe", "123456", 500);
+    console.log("Scenario 3.2 Failed");
+} catch(_) {
+    console.log("Scenario 3.2 Pass");
+}
+
+// Issue 3, Scenario 2 Alt
+try {
+    bank.withdrawMoney("John Doe", "5874321", 500);
+    console.log("Scenario 3.2 Failed");
+} catch(_) {
+    console.log("Scenario 3.2 Pass");
+}
+
+// Issue 3, Scenario 2 Alt
+try {
+    bank.withdrawMoney("Jon Doe", "123456", 500);
+    console.log("Scenario 3.2 Failed");
+} catch(_) {
+    console.log("Scenario 3.2 Pass");
+}
+
+// Issue 3, Scenario 3
+try {
+    bank.withdrawMoney("John Doe", "587432", -500);
+    console.log("Scenario 3.3 Failed");
+} catch(_) {
+    console.log("Scenario 3.3 Pass");
+}
+
+// Issue 3, Scenario 3
+try {
+    bank.withdrawMoney("John Doe", "587432", 0);
+    console.log("Scenario 3.3 Failed");
+} catch(_) {
+    console.log("Scenario 3.3 Pass");
+}
+
+try {
+    bank.withdrawMoney("John Doe", "587432", 56123);
+    console.log("Scenario 3.3 Failed");
+} catch(_) {
+    console.log("Scenario 3.3 Pass");
+}
+
+// Issue 4, Scenario 2
+try {
+    let balance = bank.checkBalance("Jon Doe", "587432");
+    console.log(balance + "Scenario 4.2 Failed");
+} catch(_) {
+    console.log("Scenario 4.2 Pass");
+}
+
+// Issue 4, Scenario 2
+try {
+    let balance = bank.checkBalance("John Doe", "5487432");
+    console.log(balance + "Scenario 4.2 Failed");
+} catch(_) {
+    console.log("Scenario 4.2 Pass");
+}
+
+// Issue 4, Scenario 2
+try {
+    let balance = bank.checkBalance("Jon Doe", "5857432");
+    console.log(balance + "Scenario 4.2 Failed");
+} catch(_) {
+    console.log("Scenario 4.2 Pass");
 }
